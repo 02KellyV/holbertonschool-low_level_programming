cf79209ce32a2d705182a1844b21e565a6fb759b
02KellyV <900@holbertonschool.com>
2019-06-19 22:33:57 +0000

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <unistd.h>
 #include <string.h>
 
@@ -9,7 +8,8 @@
  */
 int main(void)
 {
-	char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(1, message, strlen(message));
+	char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	write(1, msg, strlen(msg));
 	return (1);
 }
