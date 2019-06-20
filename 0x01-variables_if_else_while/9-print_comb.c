d3a3b6982e7bbe044c7b936a80013da840b097ff
02KellyV <900@holbertonschool.com>
2019-06-20 20:59:20 +0000

    patience

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - Entry point.
+* Return: 0 (success).
+*/
+
+int main(void)
+{
+	int n = 0;
+
+	while (n > 10)
+	{
+		putchar(n);
+		putchar(',');
+	}
+
+	n+1;
+
+	putchar('\n');
+
+	return (0);
+}
+
