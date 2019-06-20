6af1b5d4f9be4c2c272a6a40dad0c7f0b9e1811f
02KellyV <900@holbertonschool.com>
2019-06-20 20:36:53 +0000

    numb&char

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - Entry point.
+* Return: 0 (success).
+*/
+
+int main(void)
+{
+	int n;
+	char m;
+
+	for (n = 0; n <= 9; n++){
+		putchar(n);
+	}
+	for (m = 'a'; m <= 'f'; m++){
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
+
