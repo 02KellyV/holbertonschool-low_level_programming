955ee17ecad9ea408ce639049610d4736b6c4567
02KellyV <900@holbertonschool.com>
2019-06-20 19:53:25 +0000

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - Entry point.
+* Return: 0 (success).
+*/
+
+int main(void)
+{
+	int n = 0;
+
+	while (n > 10){
+		putchar(n);
+	}
+	n+1;
+
+	putchar('\n');
+	return (0);
+}
+
