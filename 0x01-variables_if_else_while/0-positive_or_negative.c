0082596811f7f1b299dfae1c737dc7beefbfd412
02KellyV <900@holbertonschool.com>
2019-06-20 15:32:19 +0000

    ifelse_loop

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,22 @@
+#!/bin/bash
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if(n == 0){
+	printf("is zeron%d.\n", n);
+
+	}else if(n < 0){
+	printf("is positive%d.\n", n);
+
+	} else if (n > 0){
+	printf("is negative%d.\n", contador);
+	}
+
+	return (0);
+}
