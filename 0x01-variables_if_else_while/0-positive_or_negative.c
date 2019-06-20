22f9beb87eb35d9140075dda6c561efdbf22e27e
02KellyV <900@holbertonschool.com>
2019-06-20 18:36:06 +0000

    alphabet-all

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,9 +5,9 @@
 int main(void)
 {
 	int n = 0;
-	int RAND = 100;
+	int RAND_MAX = 100;
 	srand(time(0));
-	n = rand() - RAND / 2;
+	n = rand() - RAND_MAX / 2;
 	if(n == 0){
 		printf("is zero %d.\n", n);
 
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z'){
+		putchar(letter++);
+	}
+	putchar('\n');
+	return (0);
+}
