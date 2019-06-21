9f55964efda5e82f7228e23245ccc70649af31cf
02KellyV <900@holbertonschool.com>
2019-06-21 02:40:51 +0000

    comb2cipher

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,17 +7,22 @@
  * Return: Always 0 (Success)
  */
 
-int main(void) {
+int main(void)
+{
+
 	int i = 48, j = 49;
+
 	while (i < 58)
 	{
 		while (j < 58)
 		{
 			putchar(i);
 			putchar(j);
-			if (i != 56|| j != 57)
+
+			if (i != 56 || j != 57)
 			{
 				putchar(',');
+				putchar(' ');
 			}
 			j++;
 		}
