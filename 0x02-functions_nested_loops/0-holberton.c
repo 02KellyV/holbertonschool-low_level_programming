c990c6cce81483fd1fc503955fc652bcfc980133
02KellyV <900@holbertonschool.com>
2019-06-24 17:26:48 +0000

    cv

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,14 +1,15 @@
-extern int _putchar(char c);
+int _putchar(char c);
 
-int main (void)
+int main(void)
 {
+
 int i;
 char h[9] = "Holberton";
 
 	for (i = 0; i < 9; i++)
 	{
-		_putchar(h[i]);
+		_putchar (h[i]);
 	}
-_putchar('\n');
-return(0);
+_putchar ('\n');
+return (0);
 }
