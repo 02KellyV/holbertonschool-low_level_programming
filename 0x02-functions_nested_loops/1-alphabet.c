b82e25dafdc7aa7eb5b6c9765b930b2c55a533ce
02KellyV <900@holbertonschool.com>
2019-06-25 05:44:17 +0000

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,25 +1,21 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
+ * print_alphabet - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
-void print_alphabet(void);
-int _putchar(char c);
 
-void print_alphabet (void)
+void print_alphabet(void)
 {
 	char alp = 'a';
 
 	while (alp <= 'z')
 	{
-
 		_putchar (alp);
-		alp ++;
+		alp++;
 
 	}
-
 	_putchar ('\n');
-	return;
+
 }
