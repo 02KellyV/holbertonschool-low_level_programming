5d07e0e8995dd70146634530f75e9ed06153fb49
02KellyV <900@holbertonschool.com>
2019-06-25 12:52:09 +0000

    fibonacci

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,42 @@
+#include "holberton.h"
+
+void print_times_table(int n)
+{
+	int i;
+	int j;
+	if (n > 0 && n < 15)
+	{
+
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < n; j++)
+			{
+
+                        _putchar(j*i);
+                        _putchar(',');
+
+				if ((i*j) < 10)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+				}
+
+				else if ((i*j) < 100)
+				{
+					_putchar(' ');
+					_putchar(' ');
+				}
+				else if ((i*j) < 1000)
+				{
+					_putchar(' ');
+				}
+			_putchar(j*i);
+			_putchar(',');
+			}
+		_putchar('\n');
+		}
+
+	}
+
+}
