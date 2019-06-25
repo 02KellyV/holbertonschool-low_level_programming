aeea92b95428133195cc843b31be0fa3e0adbb2a
02KellyV <900@holbertonschool.com>
2019-06-25 14:12:16 +0000

    gn

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,71 +1,39 @@
 #include <stdio.h>
 
-
 /**
- * sums - Entry point
+ * main - sums 3-5_cous - Entry point
  *
- * Return:  (Success)
- */
-
-int sums(int lim);
-
-
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * Return:  (0)
  */
 
 int main(void)
 {
 
-int lim  = 1024;
-int res;
-res = sums(lim);
-printf("%d", res);
-return (0);
+	int i = 1;
+	int m3 = 3;
+	int m5 = 5;
+	int sum = 0;
+	int lim = 1024;
 
-}
-
-
-
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-
-
-int sums(int lim)
-{
-
-int m3 = 3;
-int m5 = 5;
-int i = 0;
-int R = 0;
-
-while (i == 0)
-{
-
-	if (m3 < lim)
+	while (i == 0)
 	{
-		R += m3;
-		m3 += 3;
+
+		if (m3 < lim)
+		{
+			R += m3;
+			m3 += 3;
+		}
+
+		if (m5 < lim)
+		{
+			R += m5;
+			m5 += 5;
+		}
+
+		if (lim <= m3 && lim <= m5)
+		{
+			i = 1;
+		}
 	}
-
-	if (m5 < lim)
-	{
-		R += m5;
-		m5 += 5;
-	}
-
-	if (lim <= m3 && lim <= m5)
-	{
-		i = 1;
-	}
-
-}
-
-return (R);
-
+	return (0);
 }
