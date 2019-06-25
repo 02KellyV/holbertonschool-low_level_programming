77c04a88f150594567dbfccaed625b91832b9103
02KellyV <900@holbertonschool.com>
2019-06-25 13:48:15 +0000

    tbls

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,19 +1,31 @@
 #include "holberton.h"
 
+int _putchar(char c);
+
+/**
+ * print_times_tables - integer
+ *
+ * purpose
+ *
+ * Return: void
+ */
+
 void print_times_table(int n)
 {
 	int i;
 	int j;
+
 	if (n > 0 && n < 15)
 	{
 
 		for (i = 0; i < n; i++)
 		{
+
 			for (j = 0; j < n; j++)
 			{
 
-                        _putchar(j*i);
-                        _putchar(',');
+	                        _putchar(j*i);
+        	                _putchar(',');
 
 				if ((i*j) < 10)
 				{
@@ -27,14 +39,19 @@ void print_times_table(int n)
 					_putchar(' ');
 					_putchar(' ');
 				}
+
 				else if ((i*j) < 1000)
 				{
 					_putchar(' ');
 				}
+
 			_putchar(j*i);
 			_putchar(',');
+
 			}
+
 		_putchar('\n');
+
 		}
 
 	}
