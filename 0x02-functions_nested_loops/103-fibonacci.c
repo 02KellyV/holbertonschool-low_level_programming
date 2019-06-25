a5bef817c7d6403f98f50a1a7b26e31ffdff8a01
02KellyV <900@holbertonschool.com>
2019-06-25 13:38:37 +0000

    even-vals

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,29 +7,26 @@
  *
  * Return: (0) always
  */
-
-int main(void)
-{
-	unsigned long int lim = 4000000;
-	unsigned long int bef = 0;
-	unsigned long int aft = 1;
-	unsigned long int R = 0;
-	unsigned long int sum = 0;
-
-	while (lim > R)
+	int main(void)
 	{
+		unsigned long int lim = 4000000;
+		unsigned long int bef = 0;
+		unsigned long int aft = 1;
+		unsigned long int R = 0;
+		unsigned long int sum = 0;
 
-		R = bef + aft;
-
-		if ((R % 2) == 0)
+		while (lim > R)
 		{
-			sum += R;
+			R = bef + aft;
+
+			if ((R % 2) == 0)
+			{
+				sum += R;
+			}
+
+			bef = aft;
+			aft = R;
 		}
-
-		bef = aft;
-		aft = R;
-	}
-
-        printf("%li\n", sum);
-        return (0);
+		printf("%li\n", sum);
+		return (0);
 }
