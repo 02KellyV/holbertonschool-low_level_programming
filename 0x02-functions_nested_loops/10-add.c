11fce8c029be65d6c8e6b09055efdcea8a324d62
02KellyV <900@holbertonschool.com>
2019-06-26 02:52:40 +0000

    sum

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+int _putchar(char c);
+
+/**
+ * add - sum
+ *
+ * @n: an integer param
+ * @m: another integer param
+ * Return: No return
+ */
+
+int add(int n, int m)
+{
+	int sum = (n + m);
+
+	return (sum);
+}
