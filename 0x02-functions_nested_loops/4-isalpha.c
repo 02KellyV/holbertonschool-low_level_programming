4b6091f6fb3c53bd11fb2ee8a9ee7208f55d0277
02KellyV <900@holbertonschool.com>
2019-06-26 01:33:36 +0000

    alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,18 +3,15 @@
 /**
  * _isalpha - check the code for Holberton School students.
  *
- * @param c an integer argument
+ * @c: is c an integer argument
  *
  * Return: Always 0.
  */
 
 int _isalpha(int c)
 {
-	if (c >= 48 && c <= 57)
-	{
-		return (1);
-	}
-	else if (c >= 97 && c <= 122)
+
+	if (c >= 97 && c <= 122)
 	{
 		return (1);
 	}
