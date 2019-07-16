12ef640381ff612569ae332d894740679e012e94
02KellyV <900@holbertonschool.com>
2019-07-16 10:50:29 +0000

    newFix

--- 0x06-pointers_arrays_strings/102-infinite_add.c
+++ 0x06-pointers_arrays_strings/102-infinite_add.c
@@ -27,87 +27,51 @@ void rev_string(char *n)
 }
 
 /**
- * infinite_add - really infinite
- * @n1: pointer to char
- * @n2: pointer to char
- * @r: buffer parameter pointer
+ * infinite_add - add 2 numbers together
+ * @n1: text representation of 1st number to add
+ * @n2: text representation of 2nd number to add
+ * @r: pointer to buffer
  * @size_r: buffer size
- * Return: r
+ * Return: pointer to calling function
  */
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int ovr = 0;
-	int i = 0;
-	int j = 0;
-	int digits = 0;
-	int val1 = 0;
-	int val2 = 0;
-	int temp_tot = 0;
+	int overflow = 0, i = 0, j = 0, digits = 0;
+	int val1 = 0, val2 = 0, temp_tot = 0;
 
 	while (*(n1 + i) != '\0')
-	{
 		i++;
-	}
-
 	while (*(n2 + j) != '\0')
-	{
 		j++;
-	}
 	i--;
 	j--;
-
 	if (j >= size_r || i >= size_r)
-	{
 		return (0);
-	}
-
-	while (j >= 0 || i >= 0 || ovr == 1)
+	while (j >= 0 || i >= 0 || overflow == 1)
 	{
 		if (i < 0)
-		{
 			val1 = 0;
-		}
-
 		else
-		{
 			val1 = *(n1 + i) - '0';
-		}
-
 		if (j < 0)
-		{
 			val2 = 0;
-		}
-
 		else
-		{
 			val2 = *(n2 + j) - '0';
-		}
-		temp_tot = val1 + val2 + ovr;
-
+		temp_tot = val1 + val2 + overflow;
 		if (temp_tot >= 10)
-		{
-			ovr = 1;
-		}
-
+			overflow = 1;
 		else
-		{
-			ovr = 0;
-		}
-
+			overflow = 0;
 		if (digits >= (size_r - 1))
-		{
 			return (0);
-		}
 		*(r + digits) = (temp_tot % 10) + '0';
 		digits++;
 		j--;
 		i--;
 	}
 	if (digits == size_r)
-	{
 		return (0);
-	}
 	*(r + digits) = '\0';
 	rev_string(r);
 	return (r);
