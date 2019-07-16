3cf44c78d4e13b1cb6c7497ffed8cb5d3355e221
02KellyV <900@holbertonschool.com>
2019-07-16 10:13:21 +0000

    newFix

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,25 +10,16 @@
 
 char *cap_string(char *s)
 {
-	int i;
-	int j;
-
-	char sp[] = " \t\n,;.!?\"(){}";
+	int i, j;
+	char delimeters[] = " \t\n,;.!?\"(){}";
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[0] >= 97 && s[0] <= 122)
-		{
-			s[0] -= 32;
-
-				for (j = 0; sp[j] != '\0'; j++)
-				{
-					if (s[i] == sp[j] && s[i + 1] >= 97 && s[i + 1] <= 122)
-					{
-						s[i + 1] -= 32;
-					}
-				}
-		}
+			s[0] = s[0] - 32;
+				for (j = 0; delimeters[j] != '\0'; j++)
+					if (s[i] == delimeters[j] && s[i + 1] >= 97 && s[i + 1] <= 122)
+						s[i + 1] = s[i + 1] - 32;
 	}
 	return (s);
 }
