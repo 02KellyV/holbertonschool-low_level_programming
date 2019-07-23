917c9c771f2f653e9805ed03ea16ea9b8b7f99de
02KellyV <900@holbertonschool.com>
2019-07-23 02:17:59 +0000

    dog_t

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,12 +8,14 @@
  * @owner: atribute pointer to char owner's dog
  */
 
-struct dog_t
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
