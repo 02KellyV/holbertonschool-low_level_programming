313155c499a9cc15b8b7af0db33fc31c0c704584
02KellyV <900@holbertonschool.com>
2019-07-23 00:50:23 +0000

    Ohana

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,13 +2,13 @@
 #define FILE_DOG
 
 /**
- struct Dog - structure dog
+ struct dog - structure dog
  @name: atribute pointer to char name's dog
  @age: atribute float age's dog
  @owner: atribute pointer to char owner's dog
  */
 
-struct Dog
+struct dog
 {
 	char *name;
 	float age;
