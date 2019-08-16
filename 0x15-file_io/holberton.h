f2f8f96eb507f1607cf0cd6d8d47f22751e77075
Kelly Villa <900@holbertonschool.com>
2019-08-16 23:01:00 +0000

    FD cpy

---
+++ 0x15-file_io/holberton.h
@@ -0,0 +1,15 @@
+#ifndef FILE_HOLBERTON
+#define FILE_HOLBERTON
+
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
+int append_text_to_file(const char *filename, char *text_content);
+int create_file(const char *filename, char *text_content);
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif
+
