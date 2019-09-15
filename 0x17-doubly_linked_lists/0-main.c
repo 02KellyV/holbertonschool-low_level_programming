f991b38728734d3347e857c303e0882f3442511b
02KellyV <900@holbertonschool.com>
2019-09-15 22:54:17 +0000

    get info and print from nodes

--- 0x17-doubly_linked_lists/0-main.c
+++ 0x17-doubly_linked_lists/0-main.c
@@ -1,4 +1,3 @@
-julien@ubuntu:~/0x17. Doubly linked lists$ cat 0-main.c 
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
@@ -11,25 +10,25 @@ julien@ubuntu:~/0x17. Doubly linked lists$ cat 0-main.c
  */
 int main(void)
 {
-    dlistint_t *head;
-    dlistint_t *new;
-    dlistint_t hello = {8, NULL, NULL};
-    size_t n;
+	dlistint_t *head;
+	dlistint_t *new;
+	dlistint_t hello = {8, NULL, NULL};
+	size_t n;
 
-    head = &hello;
-    new = malloc(sizeof(dlistint_t));
-    if (new == NULL)
-    {
-        dprintf(2, "Error: Can't malloc\n");
-        return (EXIT_FAILURE);
-    }
-    new->n = 9;
-    head->prev = new;
-    new->next = head;
-    new->prev = NULL;
-    head = new;
-    n = print_dlistint(head);
-    printf("-> %lu elements\n", n);
-    free(new);
-    return (EXIT_SUCCESS);
+	head = &hello;
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+	{
+		dprintf(2, "Error: Can't malloc\n");
+		return (EXIT_FAILURE);
+	}
+	new->n = 9;
+	head->prev = new;
+	new->next = head;
+	new->prev = NULL;
+	head = new;
+	n = print_dlistint(head);
+	printf("-> %lu elements\n", n);
+	free(new);
+	return (EXIT_SUCCESS);
 }
--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -10,11 +10,11 @@ size_t print_dlistint(const dlistint_t *h)
 {
 	size_t n = 0;
 
-	while(h)
+	while (h)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
 		n++;
 	}
-	return(n);
+	return (n);
 }
