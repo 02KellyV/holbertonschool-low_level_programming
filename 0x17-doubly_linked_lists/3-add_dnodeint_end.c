7b350e6ff9fa56f2b6014bdc62d4eed6e41b1d48
02KellyV <900@holbertonschool.com>
2019-09-16 04:40:35 +0000

    add node end, return addres or NULL

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+
+/**
+ * *add_dnodeint_end - adds node ending doubly linked list
+ * @head: a node double pointer type dlistint_t
+ * @n: info new node
+ * Return: address new node or NULL
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+	dlistint_t *temp = *head;
+
+	if (!head)
+	{
+		return (NULL);
+	}
+	new = malloc(sizeof(dlistint_t));
+	if (!new)
+	{
+		return (NULL);
+	}
+	new->n = n;
+	new->next = NULL;
+	if (*head == NULL)
+	{
+		new->prev = NULL;
+		*head = new;
+		return (new);
+	}
+	while (temp->next)
+	{
+		temp = temp->next;
+	}
+	temp->next = new;
+	new->prev = temp;
+	return (new);
+}
