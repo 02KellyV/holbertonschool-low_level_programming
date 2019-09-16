fb88ce1010dc351e45bd3a224de9883b41828b4a
02KellyV <900@holbertonschool.com>
2019-09-16 01:02:59 +0000

    example

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,19 +1,22 @@
 #include "lists.h"
-
 /**
- * dlistint_len - number of elements for a list.
- * @h: a node pointer type dlistint_t
- * Return: number of nodes
- */
+* dlistint_len - prints all the elements of the list.
+* @h: pointer
+* Return: The elements' number of the list
+*/
 
 size_t dlistint_len(const dlistint_t *h)
 {
-	size_t n = 0;
+	size_t l = 0;
 
-	while (h)
+	if (h == NULL)
+	{
+		return (0);
+	}
+	while (h != NULL)
 	{
 		h = h->next;
-		n++;
+		l++;
 	}
-	return (n);
+	return (l);
 }
