8b9ff5650f16f2d8e17fa463ac92ddabbd785110
02KellyV <900@holbertonschool.com>
2019-09-16 04:28:13 +0000

    add node, return addres or NULL

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - adds node starting doubly linked list
+ * @head: a node double pointer type dlistint_t
+ * @n: info new node
+ * Return: address new node or NULL
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+
+	if (!head)
+	{
+		return (NULL);
+	}
+	new = malloc(sizeof(dlistint_t));
+	if (!new)
+	{
+		return (NULL);
+	}
+	new->n = n;
+	new->next = *head;
+	new->prev = NULL;
+	if (*head)
+	{
+		(*head)->prev = new;
+	}
+	*head = new;
+	return (new);
+}
