29ccbf3e46b2f608c61f2e4b38714a62daf44b78
02KellyV <900@holbertonschool.com>
2019-09-16 01:51:02 +0000

    conflict

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,9 +1,11 @@
 #include "lists.h"
 /**
+<<<<<<< HEAD
 * dlistint_len - prints all the elements of the list.
 * @h: pointer
 * Return: The elements' number of the list
 */
+
 
 size_t dlistint_len(const dlistint_t *h)
 {
