757b20ef248e48791600917112b2cc586c27ec0f
02KellyV <900@holbertonschool.com>
2019-09-16 05:15:41 +0000

    add nodes in a index

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,51 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - adds node in a index especific
+ * @h: a node double pointer type dlistint_t
+ * @idx: index for new node
+ * @n: info new node
+ * Return: dlistint_t
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	unsigned int i = 0;
+	dlistint_t *tmp, *new = NULL;
+
+	if (h == NULL)
+		return (NULL);
+	new = malloc(sizeof(dlistint_t));
+	tmp = *h;
+
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+
+	if (*h == NULL)
+	{
+		*h = new;
+		return (new);
+	}
+
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+
+	while (i < idx - 1)
+	{
+		if (tmp == NULL)
+		{
+			return (NULL);
+		}
+		tmp = tmp->next;
+		i++;
+	}
+	if (tmp->next == NULL)
+	{
+		return (add_dnodeint_end(h, n));
+	}
+	new->next = tmp->next;
+	tmp->next->prev = new;
+	tmp->next = new;
+	new->prev = tmp;
+	return (new);
+}
