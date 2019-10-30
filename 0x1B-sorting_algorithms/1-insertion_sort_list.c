c33cc76ed8c98693506fff18259f999ca334defb
02KellyV <900@holbertonschool.com>
2019-10-30 04:19:45 +0000

    insertion

--- 0x1B-sorting_algorithms/1-insertion_sort_list.c
+++ 0x1B-sorting_algorithms/1-insertion_sort_list.c
@@ -1,18 +1,4 @@
 #include "sort.h"
-
-/**
- * swap - swaps two elements
- * @ip: iterative elemnt
- * @jp: iterative +1
- */
-void swap(int *ip, int *jp)
-{
-	int temp = *ip;
-
-	*ip = *jp;
-	*jp = temp;
-}
-
 /**
  * insertion_sort - insertion sort method
  * @listint_t: The list to be printed
@@ -20,20 +6,34 @@ void swap(int *ip, int *jp)
  */
 void insertion_sort_list(listint_t **list)
 {
-    int i, key, j;
-    for (i = 1; i < n; i++)
-    {
-        key = arr[i];
-        j = i - 1;
+	listint_t *i;
 
-        /* Move elements of arr[0..i-1], that are
-        greater than key, to one position ahead
-        of their current position */
-        while (j >= 0 && arr[j] > key
-        {
-            arr[j + 1] = arr[j];
-            j = j - 1;
-        }
-        arr[j + 1] = key;
-    }
+	if(!list || (*list)->next == NULL)
+		return;
+
+	i = (*list)->next;
+	while (i)
+	{
+
+		while (i->prev != NULL && i->prev->n > i->n)
+		{
+			i->prev->next = i->next;
+			if(i->next)
+			{
+				i->next->prev = i->prev;
+			}
+			i->next = i->prev;
+			i->prev = i->prev->prev;
+			i->next->prev = i;
+			if(i->prev == NULL)
+			{
+				*list = i;
+			} else
+			{
+				i->prev->next = i;
+			}
+			print_list(*list);
+		}
+		i = i->next;
+	}
 }
