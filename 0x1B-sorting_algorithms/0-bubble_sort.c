89be8a348ce6eea37a8e7426840e0651d210592a
02KellyV <900@holbertonschool.com>
2019-10-30 01:25:37 +0000

    bubble sorting method fix print array

--- 0x1B-sorting_algorithms/0-bubble_sort.c
+++ 0x1B-sorting_algorithms/0-bubble_sort.c
@@ -37,39 +37,3 @@ void bubble_sort(int *array, size_t size)
 		}
 	}
 }
-
-/**
- * print_array - Prints an array of integers
- * @array: The array to be printed
- * @size: Number of elements in @array
- */
-void print_array(const int *array, size_t size)
-{
-	size_t i;
-
-	i = 0;
-	while (array && i < size)
-	{
-		if (i > 0)
-		{
-			printf(", ");
-		}
-		printf("%d", array[i]);
-		++i;
-	}
-	printf("\n");
-}
-
-/*
- * int main()
- * {
- *	int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
- *	size_t n = sizeof(array)/sizeof(array[0]);
- *	print_array(array, n);
- *	printf("\n");
- *	bubble_sort(array, n);
- *	printf("\n");
- *	print_array(array, n);
- *	return (0);
- * }
- */
