6c875041351cb842a4dcb27e35992d8aebbc1835
02KellyV <900@holbertonschool.com>
2019-12-05 02:18:23 +0000

    Fix betty, returns and spaces

--- 0x1D-binary_trees/0-binary_tree_node.c
+++ 0x1D-binary_trees/0-binary_tree_node.c
@@ -1,5 +1,11 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_node - function that creates a binary tree node
+ * @parent: pointer to the parent node of the node to create
+ * @value: value to put in the new node
+ * Return: pointer to the new node, or NULL on failure
+ */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *nodo = NULL;
@@ -9,7 +15,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 
 	if (parent == NULL)
 	{
-		return nodo;
+		return (nodo);
 	}
 	else if (parent->n < value)
 	{
@@ -23,7 +29,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	}
 	else
 	{
-		return NULL;
+		return (NULL);
 	}
-	return nodo;
+	return (nodo);
 }
--- 0x1D-binary_trees/binary_trees.h
+++ 0x1D-binary_trees/binary_trees.h
@@ -7,7 +7,6 @@
 
 /**
  * struct binary_tree_s - Binary tree node
- *
  * @n: Integer stored in the node
  * @parent: Pointer to the parent node
  * @left: Pointer to the left child node
@@ -15,20 +14,16 @@
  */
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 };
-
 typedef struct binary_tree_s binary_tree_t;
 
 typedef struct binary_tree_s bst_t;
-
 typedef struct binary_tree_s avl_t;
-
 typedef struct binary_tree_s heap_t;
-
 
 /*Funct*/
 void binary_tree_print(const binary_tree_t *);
