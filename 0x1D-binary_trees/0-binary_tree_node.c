ae355579225ab4c761ef5252cf03437b072d98fe
02KellyV <900@holbertonschool.com>
2019-12-06 01:56:48 +0000

    betty 0

--- 0x1D-binary_trees/0-binary_tree_node.c
+++ 0x1D-binary_trees/0-binary_tree_node.c
@@ -11,7 +11,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	binary_tree_t *nodo = NULL;
 
 	nodo = malloc(sizeof(binary_tree_t));
-	if(nodo == NULL)
+	if (nodo == NULL)
 		return (NULL);
 
 	nodo->n = value;
