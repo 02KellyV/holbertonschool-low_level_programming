b9d6673deb237d2095de3b4b0ccdbccff2113ddb
02KellyV <900@holbertonschool.com>
2020-01-27 03:27:52 +0000

    betty

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,6 +1,9 @@
 #include "hash_tables.h"
 
-/*
+/**
+* hash_table_create - creation of light of god
+* @size: - size
+* Return: - 0 if fsiled
 */
 
 hash_table_t *hash_table_create(unsigned long int size)
@@ -10,17 +13,20 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (size < 1)
 		return (NULL);
 
-	if ((h_table = malloc(sizeof(hash_table_t))) == NULL)
+	h_table = malloc(sizeof(hash_table_t));
+
+	if (h_table == NULL)
+		return (NULL);
+	h_table->array = malloc(sizeof(hash_node_t *) * size);
+
+	if (h_table->array == NULL)
 		return (NULL);
 
-	if((h_table->array = malloc(sizeof(hash_node_t *) * size)) == NULL)
-		return (NULL);
-
-	for(unsigned long int i = 0; i < size; i++)
+	for (unsigned long int i = 0; i < size; i++)
 	{
 		h_table->array[i] = NULL;
 	}
 
 	h_table->size = size;
-	return h_table;
+	return (h_table);
 }
